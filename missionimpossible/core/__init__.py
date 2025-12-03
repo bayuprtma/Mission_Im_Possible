879c08595bcd8540615ea5d10c4df089f9416507
bayuprtma <103480101+bayuprtma@users.noreply.github.com>
2025-12-03 23:27:14 +0700

    Update __init__.py

--- missionimpossible/core/__init__.py
+++ missionimpossible/core/__init__.py
@@ -1,16 +1,1 @@
-"""
-Core subpackage for MissionImPossible.
-"""
-
-from __future__ import annotations
-
-from .detector import detect_all_conflicts        # BUKAN .core.detector
-from .resolver import resolve_universal_stack
-from .installer import install_stack, validate_environment
-
-__all__ = [
-    "detect_all_conflicts",
-    "resolve_universal_stack",
-    "install_stack",
-    "validate_environment",
-]
+# missionimpossible.core package
